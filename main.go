4829cb38091e25a3a52ac8dfc9f1069767cb9eb1
Raphael Mutschler <info@raphaelmutschler.de>
2016-03-16 12:26:44 +0100

    use godeps

--- main.go
+++ main.go
@@ -1,43 +1,43 @@
 package main
 
 import (
-    "fmt"
-    "github.com/gosnatch/gosnatch/gosnatch"
-    // "github.com/gosnatch/gosnatch/selfupdate"
-    "github.com/spf13/viper"
+	"fmt"
+	"github.com/gosnatch/gosnatch/gosnatch"
+	// "github.com/gosnatch/gosnatch/selfupdate"
+	"github.com/gosnatch/gosnatch/Godeps/_workspace/src/github.com/spf13/viper"
 )
 
 func updateHandler() {
-    fmt.Println("Update Successfull!")
+	fmt.Println("Update Successfull!")
 }
 
 func main() {
 
-    gosnatch.InitConfig()
+	gosnatch.InitConfig()
 
-    if viper.GetBool("UpdateOnStart") {
-        go gosnatch.UpadeShowExceptions()
-        go gosnatch.DailySearch(true)
-    }
+	if viper.GetBool("UpdateOnStart") {
+		go gosnatch.UpadeShowExceptions()
+		go gosnatch.DailySearch(true)
+	}
 
-    go gosnatch.DailySearch(false)
+	go gosnatch.DailySearch(false)
 
-    // if viper.GetBool("AutoUpdate") {
-    //     var updater = &selfupdate.Updater{
-    //         CurrentVersion: gosnatch.VERSION,
-    //         ApiURL:         "http://updates.gosnatch.me/",
-    //         BinURL:         "http://updates.gosnatch.me/",
-    //         DiffURL:        "http://updates.gosnatch.me/",
-    //         Dir:            "update/",
-    //         CmdName:        "", // app name
-    //         Verbose:        true,
-    //         CheckInterval:  120,
-    //     }
+	// if viper.GetBool("AutoUpdate") {
+	//     var updater = &selfupdate.Updater{
+	//         CurrentVersion: gosnatch.VERSION,
+	//         ApiURL:         "http://updates.gosnatch.me/",
+	//         BinURL:         "http://updates.gosnatch.me/",
+	//         DiffURL:        "http://updates.gosnatch.me/",
+	//         Dir:            "update/",
+	//         CmdName:        "", // app name
+	//         Verbose:        true,
+	//         CheckInterval:  120,
+	//     }
 
-    //     if updater != nil {
-    //         go updater.BackgroundRun()
-    //     }
-    // }
+	//     if updater != nil {
+	//         go updater.BackgroundRun()
+	//     }
+	// }
 
-    gosnatch.StartWebserver()
+	gosnatch.StartWebserver()
 }
